503e1a6a5863d8a4ed81d650587277224638147a
Mohammad <cse22020@cemk.ac.in>
2025-09-30 22:16:42 +0530

    updated main

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
